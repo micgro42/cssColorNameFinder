113df3efac68f95ae49d8c439c13b1c9ec22ab9d
Michael Große <mic.grosse@googlemail.com>
2021-01-27 21:31:58 +0100

    chore: upgrade to vue3 and go back to vue-cli
    Add test to ensure sufficient test coverage for sonar

--- src/components/SimilarColorCard/SimilarColorCard.vue
+++ src/components/SimilarColorCard/SimilarColorCard.vue
@@ -1,24 +1,17 @@
 <template>
     <div class="similarColorCard">
         <div class="similarColorCard__header">
-            <h3>{{ colorName }}</h3> <p>Distance: {{ roundedDistance }}</p>
+            <h3>{{ colorName }}</h3>
+            <p>Distance: {{ roundedDistance }}</p>
         </div>
-        <div
-                class="similarColorCard__colorArea"
-                :style="'background-color: ' + colorName"
-        ></div>
+        <div class="similarColorCard__colorArea" :style="'background-color: ' + colorName"></div>
     </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
-interface SimilarColorCardThis {
-    distance: number;
-    colorName: string;
-    roundedDistance: () => number
-}
-
-export default {
+export default defineComponent({
     props: {
         colorName: {
             type: String,
@@ -30,29 +23,29 @@ export default {
         },
     },
     computed: {
-        roundedDistance() {
-            return Math.round((this as SimilarColorCardThis).distance);
-        }
-    }
-};
+        roundedDistance(): number {
+            return Math.round(this.distance);
+        },
+    },
+});
 </script>
 
-<style scoped lang="scss">
-    .similarColorCard {
-        margin-top: 1rem;
-        padding: 1rem;
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-        background-color: white;
-    }
+<style lang="scss">
+.similarColorCard {
+    margin-top: 1rem;
+    padding: 1rem;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    background-color: white;
+}
 
-    .similarColorCard__header {
-        display: flex;
-        justify-content: space-between;
-        align-items: baseline;
-    }
+.similarColorCard__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+}
 
-    .similarColorCard__colorArea {
-        min-height: 10vh;
-        border: 1px solid grey;
-    }
+.similarColorCard__colorArea {
+    min-height: 10vh;
+    border: 1px solid grey;
+}
 </style>
